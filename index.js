0286d20b26e6d751f033bc6173c82352dcea5c8c
Manjil41 <mahatomanjil34@gmail.com>
2024-03-05 16:26:02 +0545

    Javascript

---
+++ index.js
@@ -0,0 +1,135 @@
+// DAY_1
+// console.log("Hello world")
+
+
+
+// DAY_2
+// let a = 20;
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+// console.log(a);
+
+// const a = 30;
+// {
+//     console.log(a);
+// }
+// console.log(a);
+
+// const a = "Ram"
+
+// console.log(typeof(a));
+
+
+// const A =[1, 2, 3, 4, 5];
+
+// console.log(A[4]);
+
+//DAY_3
+
+
+// const A = ["manjil", "noone"];
+
+
+// console.log(A.indexOf("manjil"))
+
+
+
+//at()
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(fruits.join("-"));
+
+//DAY_4
+
+// const a=10;
+
+// if(a>10){
+//     console.log("value is grater then 10");
+// }
+
+// else if(a==10){
+//     console.log("value is equal to 10");
+// }
+// else{
+//     console.log("value is not gratyer than 10");
+// }
+
+//DAY_5
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// fruits.copyWithin(0, 0, 0);
+
+// console.log(fruits);
+
+// const person = { fname:"manjil", lname:"mahato", age:"21"}
+
+// let text ="";
+// for (let x in person) {
+//     console.log(text);
+//     text += person[x];
+//     //console.log(text);  preincrement value
+// }
+
+
+// const A = [1, 5, 6, 10];
+
+// console.log(A);
+//   const B = A.map((x) => x * 2);
+//   //console.log(mapping);
+
+//   console.log(B);
+
+
+// const A = 10;
+
+// const B = A.toString();
+
+// console.log(typeof(B));
+
+
+//DAY_6
+
+
+// const fruits = new Map(
+//     [
+//         ["apple",500], ["bananas", 300], ["oranges", 200],
+//     ]
+// );
+
+
+// fruits.set("apples", 200);
+
+// console.log(fruits.get("oranges"));
+
+// const fruits = new Map(
+//         [
+//             ["apple",500], ["bananas", 300], ["oranges", 200],
+//         ]
+//     );
+
+//     fruits.delete("apples");
+
+//     console.log(fruits.has("apples"));
+
+    // const fruits = new Map(
+    //     [
+    //         ["apple",500], ["bananas", 300], ["oranges", 200],
+    //     ],
+    // );
+
+    // let text = "";
+    // fruits.forEach(function (value, key) {
+    //     text += key + '=' + value + "<br>"
+    // })
+
+    // console.log(text);
+
+
+
+    //DAY_7
+
+    
